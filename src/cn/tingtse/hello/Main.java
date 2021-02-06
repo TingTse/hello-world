a91627e472d016bf905ce225fa0f10ef0bda4708
enting_xie@hotmail.com <enting_xie@hotmail.com>
2021-02-06 21:33:31 +0800

    2021-02-06 created

---
+++ src/cn/tingtse/hello/Main.java
@@ -0,0 +1,10 @@
+package cn.tingtse.hello;
+
+/**
+ * @author enting_xie 2021-02-06
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
